9071819043159879c40b576e05fdc14b7f4aeef4
gbzjzkzo <87307625+gbzjzkzo@users.noreply.github.com>
2021-07-29 13:23:07 +0700

    Create donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,13 +1,10 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [081515860089]
-│ • Telkomsel [081334177340]
+│ • Dana.     [081299609670
+│ • Telkomsel [6281299609670]
 ╰────
 
-╭─「 Donasi • Non Pulsa 」
-│ • Gopay, OVO, Dana [081515860089]
-│ • https://saweria.co/Nurutomo
-╰────
+
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
